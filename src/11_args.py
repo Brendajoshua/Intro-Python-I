a62c5343e457e06d69d53ab6821327d1f55f3c02
Brenda Joshua <brendajosh.bj@gmail.com>
2020-02-11 15:47:50 +0300

    complete task 14

--- src/11_args.py
+++ src/11_args.py
@@ -16,10 +16,14 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    sum = 0
-    for arg in args:
-        sum = sum + arg
-    return sum
+  total = 0
+  for n in args:
+    if isinstance(n, (list, tuple)):
+      for v in n:
+        total += v
+    else:
+      total += n
+  return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -44,14 +48,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-def f3(arg1, *args):
-    sum = 0
-    for arg in args:
-        sum = sum + arg
-    if sum == 0:
-        return arg1 + 1
-
-    return arg1 + sum
+def f3(n1, n2=1):
+  return f2(n1, n2)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
