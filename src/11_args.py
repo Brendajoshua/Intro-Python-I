f5892867c4cc4ce8ef03ed2f87b7fb9a0f2ab226
Brenda Joshua <brendajosh.bj@gmail.com>
2020-02-11 10:34:59 +0300

    complete task 11

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(n1, n2):
+    return n1 + n2
 
 print(f1(1, 2))
 
@@ -13,6 +15,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    sum = 0
+    for arg in args:
+        sum = sum + arg
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,6 +29,13 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
+'''
+We may use tuple(a) to convert the list to tuples, while the leading * spills the contents as arguments into the function
+like so: f2(*tuple(a))
+https://www.geeksforgeeks.org/python-convert-a-list-into-a-tuple/
+However, we can also easily use f2(*a)
+'''
+
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -30,6 +44,14 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(arg1, *args):
+    sum = 0
+    for arg in args:
+        sum = sum + arg
+    if sum == 0:
+        return arg1 + 1
+
+    return arg1 + sum
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +66,17 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(l = [], **kwargs):
+  if len(l) > 0:
+    kwargs['__list__'] = l
+  for k, v in kwargs.items():
+    if isinstance(v, dict):
+      print(f"key: {k}, value: dict(")
+      for k1, v1 in v.items():
+        print(f"  key: {k1}, value: {v1}")
+      print(")")
+    else:
+      print(f"key: {k}, value: {v}")
 
 # Should print
 # key: a, value: 12
@@ -62,4 +95,7 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+'''
+change it to a keyword argument by adding two asteriks before the variable like so **d
+'''
+f4(**d)
